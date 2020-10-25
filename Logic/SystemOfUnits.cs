9868ed7aba94ded33a04b86c6a2b32fd50e6e902
Eliise <eljuha@ttu.ee>
2020-10-25 16:11:10 +0200

    Measure and System of Units logic added.

---
+++ Logic/SystemOfUnits.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logic
+{
+    public class SystemOfUnits
+    {
+        private string name;
+
+        public SystemOfUnits(string name = "") {
+            this.name = name;
+        }
+
+        public string Name
+        {
+            get { return name ?? string.Empty; }
+            set { name = value; }
+        }
+    }
+}
