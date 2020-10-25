9521a0beabd92e7553c29a33781c877d9e2d90f6
Siim <Siim@DESKTOP-RPUCOT8>
2020-10-25 16:31:39 +0200

    Refactored old logic, so all tests pass.

--- Logic/BaseMeasure.cs
+++ Logic/BaseMeasure.cs
@@ -8,11 +8,56 @@ namespace Logic
     {
         public BaseMeasure(string name, string symbol = null) : base(name, symbol) { }
 
-        public BaseMeasure() : this(string.Empty) { }
+        public BaseMeasure() : this(string.Empty) { }       
 
-        public static int Multiply(int a, int b)
+        public Measure Multiply(BaseMeasure m, bool isDivide = false)
         {
-            return a * b;
+            var t1 = new MeasureTerm(this, 1);
+            var t2 = isDivide ? new MeasureTerm(m, -1) : new MeasureTerm(m, 1);
+            var t = new MeasureTerms { t1, t2 };
+            return new DerivedMeasure(t);
+        }
+
+        public Measure Multiply(DerivedMeasure m)
+        {
+            var t = new MeasureTerms();
+            t.Add(new MeasureTerm(this, 1));
+            foreach (var e in m.Terms)
+            {
+                t.Add(new MeasureTerm(e.Measure as BaseMeasure, e.Power));
+            }
+            return new DerivedMeasure(t);
+        }
+
+        public Measure Multiply(Measure m)
+        {
+            if (m is DerivedMeasure) return Multiply(m as DerivedMeasure);
+            return Multiply(m as BaseMeasure);
+        }
+
+        public Measure Divide(BaseMeasure m)
+        {
+            var s = Multiply(m, true);
+            return s;
+        }
+
+        public Measure Exponentiation(int i)
+        {
+            if (i == 0) return new BaseMeasure();
+            MeasureTerm t1;
+            if (i == 1)
+                t1 = new MeasureTerm(this);
+            else
+                t1 = new MeasureTerm(this, i);
+            var t = new MeasureTerms { t1 };
+            return new DerivedMeasure(t);
+        }
+
+        public Measure Reciprocal()
+        {
+            var t1 = new MeasureTerm(this, -1);
+            var t = new MeasureTerms { t1 };
+            return new DerivedMeasure(t);
         }
     }
 }
--- Logic/BaseUnit.cs
+++ Logic/BaseUnit.cs
@@ -13,5 +13,67 @@ namespace Logic
             measure = m;
             this.factor = factor;
         }
+
+        public Unit Exponentiation(int i)
+        {
+            if (i == 0) return new BaseUnit();
+            UnitTerm t1;
+            
+            t1 = new UnitTerm(this, i);
+            var t = new UnitTerms { t1 };
+            var a = new DerivedUnit(t);
+            var b = (BaseMeasure)this.measure;
+            a.Measure = b.Exponentiation(i);
+            a.Factor = Math.Pow(Factor, i);
+            Measures.Instance.Add(a.Measure);
+            return a;
+        }
+
+        public Unit Reciprocal()
+        {
+            var t1 = new UnitTerm(this, -1);
+            var t = new UnitTerms { t1 };
+            var a = new DerivedUnit(t);
+            var b = (BaseMeasure)this.measure;
+            a.Measure = b.Reciprocal();
+            return a;
+        }
+
+        public Unit Multiply(BaseUnit i, bool isDivide = false)
+        {
+            var t1 = new UnitTerm(this, 1);
+            var t2 = isDivide ? new UnitTerm(i, -1) : new UnitTerm(i, 1);
+            var t = new UnitTerms { t1, t2 };
+            var a = new DerivedUnit(t);
+            var b = (BaseMeasure)measure;
+            a.Measure = isDivide ? b.Multiply((BaseMeasure)i.Measure, true) : b.Multiply(i.Measure);
+            return a;
+        }
+
+        public Unit Multiply(DerivedUnit m)
+        {
+            var t = new UnitTerms();
+            t.Add(new UnitTerm(this, 1));
+            foreach (var e in m.Terms)
+            {
+                t.Add(new UnitTerm(e.Unit as BaseUnit, e.Power));
+            }
+            var a = new DerivedUnit(t);
+            var b = (BaseMeasure)measure;
+            a.Measure = b.Multiply((DerivedMeasure)m.Measure);
+            return a;
+        }
+
+        public Unit Multiply(Unit m)
+        {
+            if (m is DerivedUnit) return Multiply(m as DerivedUnit);
+            return Multiply(m as BaseUnit);
+        }
+
+        public Unit Divide(BaseUnit i)
+        {
+            var a = Multiply(i, true);
+            return a;
+        }        
     }
 }
