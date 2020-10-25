3a06b2dce429aa9a37b16205b52a5c7018a97d01
Eliise <eljuha@ttu.ee>
2020-10-25 16:09:08 +0200

    System of Unit Tests added.

---
+++ Tests/SystemOfUnitsTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Logic;
+
+namespace Tests.LogicTests
+{
+    [TestClass]
+    public class SystemOfUnitsTests
+    {
+
+        private SystemOfUnits s;
+        [TestInitialize]
+        public void InitTests()
+        {
+            s = new SystemOfUnits();
+        }
+
+        [TestCleanup]
+        public void CleanTests()
+        {
+            s = null;
+        }
+        [TestMethod]
+        public void ConstructorTest()
+        {
+            Assert.IsNotNull(s);
+        }
+
+        [TestMethod]
+        public void NameTest()
+        {
+            Assert.IsNotNull(s.Name);
+            s.Name = null;
+            Assert.AreEqual(String.Empty, s.Name);
+            s.Name = "Some name";
+            Assert.AreEqual("Some name", s.Name);
+        }
+    }
+}
